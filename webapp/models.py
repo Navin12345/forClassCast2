5f543b446d910f7c3789534b57b8dcf4c62a35f1
navin <navinbhaskariitkgp@gmail.com>
2018-07-01 16:06:24 +0530

    flashcard

--- webapp/models.py
+++ webapp/models.py
@@ -4,9 +4,9 @@ class Flashcard(models.Model):
     Chapter=models.CharField(max_length=20)
     Subject=models.CharField(max_length=20)
     Class=models.CharField(max_length=20)
-    Logo=models.CharField(max_length=1000)
-    Total=models.IntegerField()
-    Serial_number=models.IntegerField()
+    Link=models.CharField(max_length=1000)
+    Total_for_that_chapter=models.IntegerField()
+    Serial_Number=models.IntegerField()
     #Serial_number=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
 
     def __str__(self):
